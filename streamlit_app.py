a583c05f8f3b9233f126246ae299af6777dcb5ae
Will Luna <wluna@anaconda.com>
2024-06-29 09:49:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from datetime import datetime, timedelta 
+import nltk
 
 st.write("Hello, world!")
 st.write("Oh Brave New World, that has such people in it! with datetime")
