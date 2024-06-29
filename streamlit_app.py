90b798b30f358b21ad8026ff2ddc6e8443f43ed8
Will Luna <wluna@anaconda.com>
2024-06-29 09:47:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import altair as alt
 import numpy as np
 import pandas as pd
-import snowflake.connector
+#import snowflake.connector
 from datetime import datetime, timedelta #
 #from snowflake.snowpark.context import get_active_session # 
 
