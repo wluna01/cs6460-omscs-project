42c1763b217a2a7c51d921e3cccb51dcfa263d3e
Will Luna <wluna@anaconda.com>
2024-06-29 09:53:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,17 @@ import streamlit as st
 from datetime import datetime, timedelta 
 import nltk
 import snowflake.connector
+import os
+
+if 'SNOWFLAKE_USER' in os.environ:
+    snowflake_user = os.getenv('SNOWFLAKE_USER')
+    snowflake_password = os.getenv('SNOWFLAKE_PASSWORD')
+    snowflake_account = os.getenv('SNOWFLAKE_ACCOUNT')
+else:
+    # Fallback to Streamlit secrets in production
+    snowflake_user = st.secrets["SNOWFLAKE_USER"]
+    snowflake_password = st.secrets["SNOWFLAKE_PASSWORD"]
+    snowflake_account = st.secrets["SNOWFLAKE_ACCOUNT"]
 
 st.write("Hello, world!")
 st.write("Oh Brave New World, that has such people in it! with datetime")
