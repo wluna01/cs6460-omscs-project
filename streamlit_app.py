6c69ccafa19f1c601441d772283bc23bb38f4bbe
Will Luna <wluna@anaconda.com>
2024-06-29 09:47:15 -0700

    swapped snowflake library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import altair as alt
 import numpy as np
 import pandas as pd
-import streamlit as st
+import snowflake.connector
 from datetime import datetime, timedelta #
-from snowflake.snowpark.context import get_active_session # 
+#from snowflake.snowpark.context import get_active_session # 
 
 st.write("Hello, world!")
 st.write("Oh Brave New World, that has such people in it! with datetime")
