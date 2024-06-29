39eb0bf360edd7f82aab53dd07650d6e198253b4
Will Luna <wluna@anaconda.com>
2024-06-29 12:48:40 -0700

    Added dictionary

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import nltk
 import snowflake.connector
 import os
 from helper_functions import get_data, get_credentials
-from ui_functions import on_click_continue
+from ui_functions import on_click_continue, on_click_dictionary_lookup
 from story import Story
 
 st.title("La Tortuga Gigante")
@@ -25,6 +25,10 @@ if st.session_state.story.num_segments_displayed < st.session_state.story.total_
     st.button("Continuar", on_click=on_click_continue)
 else:
     st.title("Fin")
+
+word_to_lookup = st.text_input("Palabra Desconocida :thinking_face:", max_chars=20)
+st.button("Buscar", on_click=on_click_dictionary_lookup(word_to_lookup))
+definition_text = st.text(st.session_state.definition)
 
 st.title("Diagnostics")
 st.write("# Segments Displayed: " + str(st.session_state.story.num_segments_displayed))
--- ui_functions.py
+++ ui_functions.py
@@ -1,4 +1,16 @@
+from helper_functions import get_data
 import streamlit as st
 
 def on_click_continue():
     st.session_state.story.num_segments_displayed += 1
+
+def on_click_dictionary_lookup(word):
+    if word:
+        source_language = "es"
+        target_language = "en"
+        query = f"""select snowflake.cortex.translate('{word}', '{source_language}', '{target_language}')"""
+        translation = get_data(query)
+        #add_flashcard()
+        st.session_state.definition = translation[0][0]
+    else:
+        st.session_state.definition = ""
