a787c1cfd97f0271c09cabad26dcffd07ad0857b
Will Luna <wluna@anaconda.com>
2024-07-02 21:34:11 -0700

    added hello world audio

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import nltk
 from ui_functions import on_click_continue, on_click_dictionary_lookup
 from helper_functions import generate_identifier
 from story import Story
+from text_to_speech import text_to_speech
 
 st.session_state.story_name = st.text_input(label="Story Name (cannot change during demo)", value="la_tortuga_gigante", max_chars=20)
 if "user_name" not in st.session_state:
@@ -13,7 +14,9 @@ if "user_name" not in st.session_state:
 else:
     st.session_state.user_name = st.text_input(label="User Name (randomly generated during demo. data persists for those already used)", value=st.session_state.user_name, max_chars=20)
 
-st.title("La Tortuga Gigante")
+audio_is_on = st.toggle("Audio", True)
+
+st.title("The Giant Tortoise")
 
 # create instance of the story class
 if "story" not in st.session_state:
@@ -23,15 +26,19 @@ if "story" not in st.session_state:
 for index, row in st.session_state.story.segments.iterrows():
     if index < st.session_state.story.num_segments_displayed:
         st.write(index+1)
+        if audio_is_on:
+            audio_file = text_to_speech(row['STORY_SEGMENT_TEXT'])
+            audio_bytes = open(audio_file, 'rb').read()
+            st.audio(audio_bytes, format='audio/mp3', autoplay=True)
         st.write(row['STORY_SEGMENT_TEXT'])
 
 if st.session_state.story.num_segments_displayed < st.session_state.story.total_number_of_segments:
-    st.button("Continuar", on_click=on_click_continue)
+    st.button("Continue", on_click=on_click_continue)
 else:
-    st.title("Fin")
+    st.title("The End")
 
-word_to_lookup = st.text_input("Palabra Desconocida :thinking_face:", max_chars=20)
-st.button("Buscar", on_click=on_click_dictionary_lookup(word_to_lookup))
+word_to_lookup = st.text_input("Dictionary :thinking_face:", max_chars=20)
+st.button("Search", on_click=on_click_dictionary_lookup(word_to_lookup))
 definition_text = st.text(st.session_state.definition)
 
 with st.expander("Diagnostics"):
