ec12bfe894be1c19b47fbecc66b78b7cf57ff52f
Will Luna <wluna@anaconda.com>
2024-07-04 10:32:01 -0700

    simplified text to speech

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import nltk
 from ui_functions import on_click_continue, on_click_dictionary_lookup
 from helper_functions import generate_identifier
 from story import Story
-from text_to_speech import text_to_speech
+from text_to_speech import play_audio
 from ui_components import show_sidebar, show_highlightable_passage
 
 st.session_state.story_name = st.text_input(label="Story Name (cannot change during demo)", value="la_tortuga_gigante", max_chars=20)
@@ -33,11 +33,8 @@ for index, row in st.session_state.story.segments.iterrows():
         play_this = st.button(play_icon)
         #if the play button was clicked or auto play is on and this is the last segment
         if play_this or (auto_play and index == st.session_state.story.num_segments_displayed-1):
-            #generate and play the audio
-            audio_file = text_to_speech(row['STORY_SEGMENT_TEXT'])
-            audio_bytes = open(audio_file, 'rb').read()
-            st.audio(audio_bytes, format='audio/mp3', autoplay=True)
-
+            play_audio(row['STORY_SEGMENT_TEXT'])
+            
 if st.session_state.story.num_segments_displayed < st.session_state.story.total_number_of_segments:
     st.button("Continue", on_click=on_click_continue)
 else:
